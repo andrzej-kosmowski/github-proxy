26083e79ca4e0ce66f711e061ad53829e684f5e3
Andrzej <bazylek11@gmail.com>
2026-09-07 13:58:06 +0200

    first commit

---
+++ src/main/java/com/github_proxy/github_proxy/GithubProxyApplication.java
@@ -0,0 +1,13 @@
+package com.github_proxy.github_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubProxyApplication.class, args);
+	}
+
+}
