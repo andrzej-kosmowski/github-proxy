63775f379cfed8266a5dd5e1a37b13e1906e1c07
Andrzej <bazylek11@gmail.com>
2026-09-07 16:39:39 +0200

    feat: repository model created

---
+++ src/main/java/com/github_proxy/model/Repository.java
@@ -0,0 +1,31 @@
+package com.github_proxy.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "repositories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Repository {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String description;
+    private String cloneUrl;
+    private int stars;
+    private LocalDateTime createdAt;
+
+    public Repository(String fullName, String description, String cloneUrl, int stars, LocalDateTime createdAt) {
+        this.fullName = fullName;
+        this.description = description;
+        this.cloneUrl = cloneUrl;
+        this.stars = stars;
+        this.createdAt = createdAt;
+    }
+}
