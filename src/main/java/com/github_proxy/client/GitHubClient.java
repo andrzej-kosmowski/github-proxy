1957698e089c1ff57c84f86b2d2d39bfdc10e334
Andrzej <bazylek11@gmail.com>
2026-09-10 15:47:19 +0200

    feat: implement fallback factory for github client

--- src/main/java/com/github_proxy/client/GitHubClient.java
+++ src/main/java/com/github_proxy/client/GitHubClient.java
@@ -2,11 +2,13 @@ package com.github_proxy.client;
 
 import com.github_proxy.configuration.GitHubFeignConfig;
 import com.github_proxy.dto.GitHubRepository;
+import com.github_proxy.fallback.GitHubClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "github-client", url = "${app.githubClient.url}", configuration = GitHubFeignConfig.class)
+@FeignClient(name = "github-client", url = "${app.githubClient.url}", configuration = GitHubFeignConfig.class,
+        fallbackFactory = GitHubClientFallbackFactory.class)
 public interface GitHubClient {
     @GetMapping("/{owner}/{repo}")
     GitHubRepository getRepository(@PathVariable("owner") String owner, @PathVariable("repo") String repo);
